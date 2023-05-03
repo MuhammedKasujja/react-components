cdce26d82afc7b1305d284ad587ac4523decd24e
Muhammed Kasujja <al.kasmud.2@gmail.com>
2023-05-03 18:20:30 +0300

    feat: FileInput image preview

--- src/components/file-input/FileInput.tsx
+++ src/components/file-input/FileInput.tsx
@@ -1,31 +1,61 @@
 import { mergeClassNames } from "src/utils/utils";
 import classes from "./FileInput.module.scss";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 
 type FileInputProps = {
   hint?: string;
   title: string;
+  preview?: boolean;
   handleChange(file: File | null | undefined): void;
 };
 
-const FileInput: React.FC<FileInputProps> = ({ hint, title, handleChange }) => {
+const defaultFileInputProps: FileInputProps = {
+  hint: undefined,
+  title: "",
+  preview: true,
+  handleChange: (_) => {},
+};
+
+const FileInput: React.FC<FileInputProps> = ({
+  hint,
+  title,
+  handleChange,
+  preview,
+}) => {
+  const [filePreview, setFilePreview] = useState("");
   const onFileChange = (event: ChangeEvent<HTMLInputElement>) => {
-    handleChange(event.target.files?.item(0))
+    const file = event.target.files?.item(0);
+    handleChange(file);
+    if (file) {
+      setFilePreview(URL.createObjectURL(file));
+    }
   };
   return (
     <>
       <label
         className={mergeClassNames(
           classes.file_input,
-          classes.file_input_label
+          preview ? classes.file_input_preview : classes.file_input_label
         )}
         htmlFor="file_input"
       >
-        {title}
+        {preview ? (
+          filePreview ? (
+            <img
+              src={filePreview}
+              alt="selected image"
+              className={mergeClassNames(classes.placeholder)}
+            />
+          ) : (
+            <p className={mergeClassNames(classes.placeholder)}>{title}</p>
+          )
+        ) : (
+          title
+        )}
       </label>
       <input
         className={mergeClassNames(
-          classes.file_input,
+          preview ? "hidden" : classes.file_input,
           classes.file_input_input
         )}
         aria-describedby="file_input_help"
@@ -39,7 +69,7 @@ const FileInput: React.FC<FileInputProps> = ({ hint, title, handleChange }) => {
             classes.file_input,
             classes.file_input_hint
           )}
-          id="file_input_help"
+          id="file_input"
         >
           {hint}
           {/* SVG, PNG, JPG or GIF (MAX. 800x400px). */}
@@ -48,5 +78,7 @@ const FileInput: React.FC<FileInputProps> = ({ hint, title, handleChange }) => {
     </>
   );
 };
+
+FileInput.defaultProps = defaultFileInputProps;
 
 export default FileInput;
